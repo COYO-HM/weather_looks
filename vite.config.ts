c948727f10821fd3f1b3a284f987adab174438f6
COYO <dayeon.yoon@gmail.com>
2023-06-30 23:38:16 +0900

    CONFIG: VanillaExtract&&Axios install

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vanillaExtractPlugin({
+      // configuration
+    }),
+  ],
+  resolve: {
+    alias: [
+      { find: '@services', replacement: '/src/services' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
+});
